9b297e2dff89a591099fc65997ac6e74ca3e5c91
PeterQ93 <pq1993@live.ie>
2022-12-05 20:59:31 +0000

    add getNewQuestion function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -41,12 +41,45 @@ const MAX_QUESTIONS = 3;
 /**
  * Starts New Game
  */
-function startGame = () => {
+function startGame()  {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
 };
 
+/**
+ * Checks remaining questions and gets new question
+ */
+function getNewQuestion() {
+    if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
+        return window.location.assign('/end.html')
+    }
+   
+    questionCounter++;
+    let questionIndex = Math.floor(Math.random()* availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
 
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
+
+startGame();
