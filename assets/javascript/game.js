bcd48174492fc246bf3cc3cc7c23b10e579ee1f8
PeterQ93 <pq1993@live.ie>
2022-12-06 19:25:21 +0000

    add hud to be able to be updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -57,6 +59,8 @@ function getNewQuestion() {
     }
    
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     let questionIndex = Math.floor(Math.random()* availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -78,8 +82,22 @@ choices.forEach(choice => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
         console.log(selectedAnswer);
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
         getNewQuestion();
+        }, 1000);
     });
 });
+
+function incrementScore (num) {
+    score += num;
+    scoreText.innerText = score;
+    
+}
 
 startGame();
