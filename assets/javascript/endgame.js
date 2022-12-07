7277a7f3646c5cf869acb23af68114c23b81bcc4
PeterQ93 <pq1993@live.ie>
2022-12-07 20:24:13 +0000

    add the top 5 highscores to local storage for use in highscores page

--- assets/javascript/endgame.js
+++ assets/javascript/endgame.js
@@ -4,7 +4,7 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-console.log(highScores);
+
 
 finalScore.innerText = mostRecentScore;
 
@@ -17,10 +17,15 @@ function saveHighScore(event) {
     event.preventDefault();
 
     const score = {
-        score: mostRecentScore,
+        score: Math.floor(Math.random() * 100),
         name: username.value
     }
     highScores.push(score);
-    console.log(highScores);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+   
 
 }
